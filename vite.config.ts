387f317114da90f0f4e49179cb4195e8ceb5c556
Dev <dev@example.com>
2026-02-23 17:48:29 +0800

    fix: update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sound-architect/',
+  base: '/wooof-sound-architect/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
