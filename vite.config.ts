b7227418af357a3579f3579862bff7868e0af544
Dev <dev@example.com>
2026-02-23 20:33:21 +0800

    修复 GitHub Pages 资源路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wooof-sound-architect/',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
